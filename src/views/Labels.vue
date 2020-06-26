047cbea12116b7d81f9a615352258f77a74b5483
Justcho <1620845677@qq.com>
2020-06-26 16:22:32 +0800

    创建 <Layout />

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Labels.vue
-    </div>
-    <Nav/>
-  </div>
+ <Layout>Labels.vue</Layout>
+
 </template>
 
 <script lang="ts">
@@ -14,17 +10,3 @@
     name: 'Labels',
   };
 </script>
-
-<style lang="scss" scoped>
-  .nav-wrapper {
-    border: 1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
-  .content{
-    border: 1px solid blue;
-    overflow: auto;
-    flex-grow: 1;
-  }
-</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Money.vue
-    </div>
-
-    <Nav/>
-  </div>
+<Layout>
+  <p>Money.vue</p>
+</Layout>
 </template>
 
 <script lang="ts">
@@ -14,17 +10,3 @@
     name: 'Money',
   };
 </script>
-
-<style lang="scss" scoped>
-  .nav-wrapper {
-    border: 1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
-  .content{
-    border: 1px solid blue;
-    overflow: auto;
-    flex-grow: 1;
-  }
-</style>
