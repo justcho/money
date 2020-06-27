d05104c66c43b508bfaf7ca23d1850fc68921cb1
Justcho <1620845677@qq.com>
2020-06-27 17:33:22 +0800

    模块化：把Money.vue分成四个小组件

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,10 @@
   body{
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    color: #333333;
-    line-height: 1.5;
+    color: #333;
     font-family: $font-hei;
+    font-size: 16px;
+    line-height: 1.5;
+
   }
 </style>
