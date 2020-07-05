72336a970be417405d44212cf46aad69e68dd822
caolanglang <1620845677@qq.com>
2020-07-05 08:59:25 +0800

    可以选中或取消tag

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,35 +1,77 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{output || '0'}}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button><Icon name="delete"/></button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="remove"><Icon name="delete"/></button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear"><Icon name="clear"/> </button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
+      <button @click="ok" class="ok">OK</button>
+      <button @click="inputContent" class="zero">0</button>
+      <button @click="inputContent">.</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'NumberPad'
-  };
+  import Vue from 'vue';
+  import {Component,Prop} from 'vue-property-decorator';
+
+  @Component
+  export default class NumberPad extends Vue {
+             output = '0';
+
+          inputContent(event: MouseEvent){
+            const button = (event.target as HTMLButtonElement);
+              const input  = button.textContent!;
+              if(this.output.length === 16){return;}
+              if(this.output === '0'){
+                  if(input === '0'){return; }
+                  if('0123456789'.indexOf(input)>=0){
+                   this.output = input;
+                  }else{
+                    this.output += input;
+                  }
+                  return;
+              }
+              if (this.output.indexOf('.')>=0 && input === '.' ){
+                  return;
+                }
+
+                this.output += input;
+
+            }
+            remove(){
+            if(this.output.length === 1){
+              this.output = '0';
+            }else{
+              this.output = this.output.slice(0,-1)
+            }
+            }
+            clear(){
+                this.output = '0';
+            }
+            ok() {
+              const number = parseFloat(this.output);
+              this.$emit('update:number', number);
+              this.$emit('submit', number);
+              this.output = '0'
+            }
+      }
 </script>
 
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
 
   .numberPad {
+
     .output {
       @extend %clearFix;
       @extend %innerShadow;
@@ -38,6 +80,7 @@
       padding: 9px 16px;
       text-align: right;
       color: #E92525;
+      height: 72px;
     }
 
     .buttons {
