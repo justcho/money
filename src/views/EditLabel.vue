51fdf5987d231367e6ba2759250e164abd037fdb
caolanglang <1620845677@qq.com>
2020-07-06 20:22:09 +0800

    将 tag 的 string 改为 {id,name}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,20 @@
+<template>
+    <Layout>
+        编辑标签
+    </Layout>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class EditLable extends Vue {
+
+  }
+
+</script>
+
+<style scoped>
+
+</style>
