b260c2fc89cba3e347d875163a1f1123169933cf
caolanglang <1620845677@qq.com>
2020-07-10 21:48:16 +0800

    使用全局 tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,19 @@ type RecordItem = {
   amount: number; //数据类型
   createdAt?: Date; //类 / 构造函数
 }
+
+type Tag = {
+  id: string;
+  name: string;
+}
+type TagListModel = {
+  data: Tag[];
+  fetch: () => Tag[];
+  create: (name: string) => 'success' | 'duplicated'; //success表示成功 duplicated表示name重复
+  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+  remove: (id: string) => boolean;
+  save: () => void;
+}
+interface Window {
+  tagList: Tag[];
+}
